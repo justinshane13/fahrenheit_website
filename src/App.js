7447961215c9d2266cf8b6428fd1eb680e15a1b4
justinshane13 <justinshane13@gmail.com>
2022-11-16 20:45:38 -0800

    update header images; refactor categories and move to under header

--- src/App.js
+++ src/App.js
@@ -17,33 +17,56 @@ function App() {
       <Header />
       <Navbar />
       <div className='header_top'>
-        <img src='/images/Header_Dark.png' alt='several croissants on a baking tray' className='header_img' />
+        <div className='landing_bread_container'>
+          <img className='landing_bread_img1' src='/images/landing_bread4.jpg' alt='several croissants on a baking tray' />
+          <img className='landing_bread_img2' src='/images/landing_bread5.jpg' alt='several croissants on a baking tray' />
+          <img className='landing_bread_img3' src='/images/landing_bread6.jpg' alt='several croissants on a baking tray' />
+        </div>
         <div className='slogan_container'>
           <p className='slogan'>Authentic.</p>
           <p className='slogan'>Housemade.</p>
           <p className='slogan'>American desserts, pastries, and bread in the heart of Larnaca.</p>
         </div>
       </div>
-      <h2 className='body_title'>Testimonials</h2>
+      <h2 className='body_title'>Get to know our bakery</h2>
+      <div className='categories_links'>
+          <div className='category_box'>
+            <a href='www.google.com'>
+              <img src='/images/Savory5.png' alt='' className='categories_image' />
+              <p>sweet</p>
+            </a>
+          </div>
+          <div className='category_box'>
+            <a href='www.google.com'>
+              <img src='/images/Savory5.png' alt='' className='categories_image' />
+              <p>savory</p>
+            </a>
+          </div>
+          <div className='category_box'>
+            <a href='www.google.com'>
+              <img src='/images/Savory5.png' alt='' className='categories_image' />
+              <p>bread</p>
+            </a>
+          </div>
+          <div className='category_box'>
+            <a href='www.google.com'>
+              <img src='/images/Savory5.png' alt='' className='categories_image' />
+              <p>story</p>
+            </a>
+          </div>
+        </div>
+        <div className='categories'>
+          <div className='categories_text'>
+            <p className='categories_text_1'>Are you in the mood for . . .</p>
+            <div className='categories_text_vertical'>
+              <p className='categories_text_2'>something sweet?</p>
+              <p className='categories_text_2'>something savory?</p>
+              <p className='categories_text_2'>fresh-baked bread?</p>
+            </div>
+          </div>
+        </div>
+      <h2 className='body_title'>What our customers have to say</h2>
       <Reviews />
-      <div className='categories'>
-        <div>
-          <p>Image 1</p>
-          <p>Link 1</p>
-        </div>
-        <div>
-          <p>Image 2</p>
-          <p>Link 2</p>
-        </div>
-        <div>
-          <p>Image 3</p>
-          <p>Link 3</p>
-        </div>
-        <div>
-          <p>Image 4</p>
-          <p>Link 4</p>
-        </div>
-      </div>
       {bodyHtml}
       <Slideshow />
       <Footer />
