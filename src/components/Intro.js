3330ec30248ff7560ba7b0a6cd1673cc894293dd
justinshane13 <justinshane13@gmail.com>
2022-11-28 20:24:39 -0800

    update social section and recreate social media image

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -14,9 +14,11 @@ const Intro = () => {
             <div className='social'>
                 <div className='social_text_container'>
                     <p className='social_text'>The best way to keep up with Fahrenheit Bakery is on Instagram!</p>
-                    <p className='social_text'>We share what we're baking, our weekly bread schedule, hours of operation, and more. Tap or click on the phone to follow us!</p>
+                    <p className='social_text'>We share what we're baking, our weekly bread schedule, hours of operation, and more.</p>
                 </div>
-                <a href='https://www.instagram.com/fahrenheitbakery_cy/' target="_blank" rel='noopener noreferrer'><img src='/images/Fahrenheit_Instagram_Mockup.png' alt='fahrenheit instagram page screenshot' className='social_mockup'/></a>
+                <div className='social_image_container'>
+                    <a href='https://www.instagram.com/fahrenheitbakery_cy/' target="_blank" rel='noopener noreferrer'><img src='/images/social_media_callout.jpg' alt='fahrenheit instagram page screenshot' className='social_mockup'/><div className='social_image_text'><span className='social_image_text_inner'>Follow us on Instagram!</span></div></a>
+                </div>
             </div>
         </div>
     )
